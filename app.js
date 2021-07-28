3e674dbcbf4fe14a6e516921dd8c87a8b77481b2
Riddler <2584275@gmail.com>
2021-07-28 12:12:19 +0300

    подключился к Mongo

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
