da9a471433ed830bd41734bc613880bc6ff5371f
Riddler <2584275@gmail.com>
2021-07-29 19:46:16 +0300

    что-то есть

--- app.js
+++ app.js
@@ -14,7 +14,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6102985e3d303929009bad65', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
 app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- controllers/users.js
+++ controllers/users.js
@@ -2,22 +2,56 @@ const User = require('../models/user');
 
 const getUsers = (req, res) => {
   User.find({})
-    .then(users => res.status(200).send({ data: users }))
+    .then((users) => res.status(200).send(users))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
-const getUserById= (req, res) => {
-  const { userId } = req.body;
-  return User.findById ({ userId })
-    .then(user => res.status(200).send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }))
-};
+const getUserById = (req, res) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) { res.status(404).send({ message: 'Пользователь по указанному _id не найден.'});
+  return;
+    }
+      res.status(200).send(user);
+  })
+    .catch(() => {res.status(500).send({ message: 'Произошла ошибка' });
+  });
+  };
+
 
 const createUser = (req, res) => {
-  const { name, about, avatar} = req.body;
+  const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(user => res.status(200).send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => res.status(200).send(user))
+    .catch(() => {
+      if (res.status(400)) {
+        res.status(400).send({ message: 'Переданы некорректные данные для создания пользователя.' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка' });
+    })
 };
 
-module.exports = { getUsers, getUserById, createUser };
+const updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  return User.findByIdAndUpdate(req.user._id, {name, about })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  return User.findByIdAndUpdate(req.user._id, { avatar })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+    });
+};
+
+module.exports = { getUsers, getUserById, createUser, updateProfile, updateAvatar };
