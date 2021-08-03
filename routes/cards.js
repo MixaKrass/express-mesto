2820c5252c6b227eac2292ab3bfd9cb06be0c3ad
Riddler <2584275@gmail.com>
2021-08-03 18:24:34 +0300

    13.2.2

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,5 @@
+/* eslint-disable linebreak-style */
 const router = require('express').Router();
-
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
+/* eslint-disable linebreak-style */
 const router = require('express').Router();
-
 const {
   getUsers, getUserById, createUser, updateProfile, updateAvatar,
 } = require('../controllers/users');
