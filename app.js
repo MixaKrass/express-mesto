1b06f9dec91605bffa68de16c95396e270ed9b18
Riddler <2584275@gmail.com>
2021-08-11 17:14:24 +0300

    logger done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,6 +19,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.use(requestLogger); // подключаем логгер запросов
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -41,6 +44,8 @@ app.use(auth);
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 app.use('*', require('./routes/NotFound'));
+
+app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errors());
 app.use((err, req, res, next) => {
